5d8c37f874b77455b848afdedace16d51570c7f2
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2018-06-08 13:36:24 +0200

    Add tests for organizations in admin.

--- src/test/scala/ch/epfl/bluebrain/nexus/tests/BaseSpec.scala
+++ src/test/scala/ch/epfl/bluebrain/nexus/tests/BaseSpec.scala
@@ -3,22 +3,24 @@ package ch.epfl.bluebrain.nexus.tests
 import java.util.regex.Pattern.quote
 
 import akka.http.javadsl.model.headers.HttpCredentials
+import akka.http.scaladsl.model.HttpMethods.PATCH
 import akka.http.scaladsl.model.Uri.{Path => AkkaPath}
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Authorization
+import akka.http.scaladsl.model.{RequestEntity, StatusCodes, HttpRequest => Req, _}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import ch.epfl.bluebrain.nexus.commons.http.HttpClient
 import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
+import ch.epfl.bluebrain.nexus.commons.http.JsonLdCirceSupport._
 import ch.epfl.bluebrain.nexus.commons.test.{Randomness, Resources}
 import ch.epfl.bluebrain.nexus.tests.config.Settings
 import com.typesafe.config.ConfigFactory
 import io.circe.Json
 import io.circe.parser._
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{Assertion, Matchers, WordSpecLike}
+import org.scalatest.{Assertion, Matchers, OptionValues, WordSpecLike}
 
 import scala.collection.immutable.Seq
 import scala.concurrent.duration._
@@ -30,7 +32,8 @@ class BaseSpec
     with ScalatestRouteTest
     with ScalaFutures
     with Resources
-    with Randomness {
+    with Randomness
+    with OptionValues {
 
   private[tests] val config                        = new Settings(ConfigFactory.parseResources("test-app.conf").resolve()).appConfig
   private[tests] val credGroup                     = HttpCredentials.createOAuth2BearerToken(config.iam.groupToken)
@@ -40,6 +43,39 @@ class BaseSpec
   private[tests] val errorCtx                      = Map(quote("{error-context}") -> config.prefixes.errorContext.toString)
   private[tests] val resourceCtx                   = Map(quote("{success-context}") -> config.prefixes.coreContext.toString)
   private[tests] val resourceIamCtx                = Map(quote("{success-iam-context}") -> config.iam.coreContext.toString)
+  private[tests] val adminBase                     = config.admin.uri
+  private[tests] val iamBase                       = config.iam.uri
+  private[tests] val replSub                       = Map(quote("{sub}") -> config.iam.userSub)
+
+  def cleanAcls = {
+
+    cl(Req(uri = s"$iamBase/acls/*/*?parents=true", headers = headersGroup)).mapJson { (json, result) =>
+      result.status shouldEqual StatusCodes.OK
+      val permissions =
+        json.hcursor.get[Vector[Json]]("acl").toOption.value.foldLeft(Set.empty[(String, Set[String])]) {
+          case (acc, j) =>
+            j.hcursor.downField("identity").get[String]("sub") match {
+              case Right(s) if s == config.iam.userSub =>
+                acc + (j.hcursor.get[String]("path").toOption.value ->
+                  j.hcursor.get[Set[String]]("permissions").toOption.value)
+              case _ => acc
+            }
+        }
+
+      permissions.foreach {
+        case (path, perms) =>
+          val entity =
+            jsonContentOf("/iam/patch-single.json", replSub + (quote("{perms}") -> perms.mkString("""",""""))).toEntity
+          cl(Req(PATCH, s"$iamBase/acls$path", headersGroup, entity)).mapJson { (json, result) =>
+            result.status shouldEqual StatusCodes.OK
+            json shouldEqual jsonContentOf("/iam/patch-response.json", replSub ++ resourceIamCtx)
+          }
+      }
+
+      result.status shouldEqual StatusCodes.OK
+
+    }
+  }
 
   override implicit def patienceConfig: PatienceConfig = PatienceConfig(config.http.patienceConfig, 300 millis)
 
@@ -52,6 +88,13 @@ class BaseSpec
 
   private[tests] implicit class JsonSyntax(json: Json) {
     def toEntity: RequestEntity = HttpEntity(ContentTypes.`application/json`, json.noSpaces)
+
+    def getString(field: String): String   = json.asObject.flatMap(_(field)).flatMap(_.asString).get
+    def getLong(field: String): Long       = json.asObject.flatMap(_(field)).flatMap(_.asNumber).flatMap(_.toLong).get
+    def getBoolean(field: String): Boolean = json.asObject.flatMap(_(field)).flatMap(_.asBoolean).get
+    def getJson(field: String): Json       = json.asObject.flatMap(_(field)).get
+
+    def updateField(field: String, value: String): Json = json.mapObject(_.add(field, Json.fromString(value)))
   }
 
   private[tests] implicit class HttpResponseSyntax(value: Future[HttpResponse])(
@@ -67,6 +110,56 @@ class BaseSpec
   private[tests] implicit class RequestEntitySyntax(entity: RequestEntity) {
     def toJson: Option[Json] =
       entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.utf8String).map(parse).futureValue.toOption
+  }
+  private[tests] val startPool = Vector.range('a', 'z')
+  private[tests] val pool      = Vector.range('a', 'z') ++ Vector.range('0', '9') :+ '_' :+ '-'
+
+  private[tests] def randomProjectPrefix = genString(1, startPool) + genString(genInt(10), pool)
+
+  private[tests] def genId(length: Int = 15): String =
+    genString(length = length, Vector.range('a', 'z') ++ Vector.range('0', '9'))
+
+  private[tests] def projectReqEntity(
+      path: String = "/admin/projects/create.json",
+      nxv: String = randomProjectPrefix,
+      person: String = randomProjectPrefix,
+      name: String = genString(),
+      base: String = s"${config.admin.uri.toString()}/${genString()}"): RequestEntity = {
+    val rep = Map(quote("{nxv-prefix}") -> nxv,
+                  quote("{person-prefix}") -> person,
+                  quote("{name}")          -> name,
+                  quote("{base}")          -> base)
+    jsonContentOf(path, rep).toEntity
+  }
+
+  private[tests] def orgReqEntity(name: String = genString()): RequestEntity = {
+    val rep = Map(quote("{name}") -> name)
+    jsonContentOf("/admin/orgs/payload.json", rep).toEntity
+  }
+
+  private[tests] def createRespJson(id: String, rev: Long, tpe: String = "projects"): Json = {
+    val resp = resourceCtx ++ Map(quote("{base}") -> config.admin.uri.toString(),
+                                  quote("{id}")        -> id,
+                                  quote("{type}")      -> tpe,
+                                  quote(""""{rev}"""") -> rev.toString)
+    jsonContentOf("/admin/response.json", resp)
+  }
+
+  private[tests] def validateAdminResource(json: Json,
+                                           tpe: String,
+                                           idPrefix: String,
+                                           id: String,
+                                           name: String,
+                                           rev: Long,
+                                           deprecated: Boolean = false) = {
+    json.getString("@context") shouldEqual config.prefixes.coreContext.toString()
+    json.getString("@id") shouldEqual s"${config.admin.uri.toString()}/$idPrefix/$id"
+    json.getString("@type") shouldEqual s"nxv:$tpe"
+    json.getString("_uuid") should fullyMatch regex """[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"""
+    json.getString("label") shouldEqual id
+    json.getString("name") shouldEqual name
+    json.getLong("_rev") shouldEqual rev
+    json.getBoolean("_deprecated") shouldEqual deprecated
   }
 
 }
---
+++ src/test/scala/ch/epfl/bluebrain/nexus/tests/admin/OrgsSpec.scala
@@ -0,0 +1,272 @@
+package ch.epfl.bluebrain.nexus.tests.admin
+
+import java.util.regex.Pattern.quote
+
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.{StatusCodes, HttpRequest => Req}
+import ch.epfl.bluebrain.nexus.commons.http.JsonLdCirceSupport._
+import ch.epfl.bluebrain.nexus.commons.http.JsonOps._
+import ch.epfl.bluebrain.nexus.tests.BaseSpec
+import io.circe.Json
+import org.scalatest.concurrent.Eventually
+import org.scalatest.{CancelAfterFailure, OptionValues}
+
+class OrgsSpec extends BaseSpec with OptionValues with CancelAfterFailure with Eventually {
+
+  "managing ACLs" should {
+
+    "delete all ACLs for user" in cleanAcls
+  }
+
+  "creating an organization" should {
+
+    "fail if the permissions are missing" in {
+      eventually {
+        cl(Req(PUT, s"$adminBase/orgs/${genId()}", headersUser, Json.obj().toEntity)).mapJson { (json, result) =>
+          result.status shouldEqual StatusCodes.Unauthorized
+          json shouldEqual jsonContentOf("/iam/errors/unauthorized-access.json", errorCtx)
+        }
+      }
+    }
+
+    "add organizations/create permissions for user" in {
+      val json = jsonContentOf("/iam/add.json", replSub + (quote("{perms}") -> "orgs/create")).toEntity
+      cl(Req(PUT, s"$iamBase/acls/", headersGroup, json)).mapResp { result =>
+        result.status shouldEqual StatusCodes.OK
+        result.entity.isKnownEmpty() shouldEqual true
+      }
+    }
+
+    "fail if the organizations name is missing" in {
+      eventually {
+        cl(Req(PUT, s"$adminBase/orgs/${genId()}", headersUser, Json.obj().toEntity)).mapJson { (json, result) =>
+          result.status shouldEqual StatusCodes.BadRequest
+          json.removeKeys("violations") shouldEqual jsonContentOf("/admin/errors/create-no-name-resp.json", errorCtx)
+        }
+      }
+    }
+
+    "succeed if payload is correct" in {
+      val id = genId()
+      cl(Req(PUT, s"$adminBase/orgs/$id", headersUser, orgReqEntity("/admin/orgs/payload.json"))).mapJson {
+        (json, result) =>
+          result.status shouldEqual StatusCodes.Created
+          json shouldEqual createRespJson(id, 1L, "orgs")
+      }
+    }
+
+    "fail if organization already exists" in {
+      val id = genId()
+      cl(Req(PUT, s"$adminBase/orgs/$id", headersUser, orgReqEntity("/admin/orgs/payload.json"))).mapJson {
+        (json, result) =>
+          result.status shouldEqual StatusCodes.Created
+          json shouldEqual createRespJson(id, 1L, "orgs")
+      }
+
+      cl(Req(PUT, s"$adminBase/orgs/$id", headersUser, orgReqEntity("/admin/orgs/payload.json"))).mapJson {
+        (json, result) =>
+          result.status shouldEqual StatusCodes.Conflict
+          json shouldEqual jsonContentOf("/admin/errors/already-exists.json", errorCtx)
+      }
+
+    }
+
+    "fail when the organization segment is illegal" in {
+      cl(Req(PUT, s"$adminBase/orgs/123K=", headersUser, orgReqEntity("/admin/orgs/payload.json"))).mapJson {
+        (json, result) =>
+          result.status shouldEqual StatusCodes.BadRequest
+          json shouldEqual jsonContentOf("/admin/errors/illegal-param-org.json", errorCtx)
+      }
+    }
+  }
+
+  "fetching an organization" should {
+    val id     = genId()
+    val name   = genString()
+    val create = orgReqEntity(name)
+    "fail if the permissions are missing" in {
+
+      cl(Req(PUT, s"$adminBase/orgs/$id", headersUser, create)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.Created
+        json shouldEqual createRespJson(id, 1L, "orgs")
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.Unauthorized
+        json shouldEqual jsonContentOf("/iam/errors/unauthorized-access.json", errorCtx)
+      }
+    }
+
+    "add orgs/read permissions for user" in {
+      val json = jsonContentOf("/iam/add.json", replSub + (quote("{perms}") -> "orgs/read")).toEntity
+      cl(Req(PUT, s"$iamBase/acls/", headersGroup, json)).mapResp { result =>
+        result.status shouldEqual StatusCodes.OK
+        result.entity.isKnownEmpty() shouldEqual true
+      }
+    }
+
+    "succeed if organization exists" in {
+      eventually {
+        cl(Req(uri = s"$adminBase/orgs/$id", headers = headersUser)).mapJson { (json, result) =>
+          result.status shouldEqual StatusCodes.OK
+          validateAdminResource(json, "Organization", "orgs", id, name, 1L)
+
+        }
+      }
+    }
+
+    "return not found when fetching a non existing revision of an organizations" in {
+      cl(Req(uri = s"$adminBase/orgs/$id?rev=3", headers = headersUser)).mapResp { result =>
+        result.status shouldEqual StatusCodes.NotFound
+      }
+    }
+
+    "return not found when fetching a non existing organization" in {
+      eventually {
+        cl(Req(uri = s"$adminBase/orgs/${genId()}", headers = headersUser)).mapResp { result =>
+          result.status shouldEqual StatusCodes.NotFound
+        }
+      }
+    }
+  }
+
+  "updating an organization" should {
+
+    val id     = genId()
+    val name   = genString()
+    val create = orgReqEntity(name)
+
+    "fail if the permissions are missing" in {
+
+      cl(Req(PUT, s"$adminBase/orgs/$id", headersUser, create)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.Created
+        json shouldEqual createRespJson(id, 1L, "orgs")
+      }
+
+      cl(Req(PUT, s"$adminBase/orgs/$id?rev=1", headersUser, create)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.Unauthorized
+        json shouldEqual jsonContentOf("/iam/errors/unauthorized-access.json", errorCtx)
+      }
+
+    }
+
+    "add orgs/write permissions for user" in {
+      val json = jsonContentOf("/iam/add.json", replSub + (quote("{perms}") -> "orgs/write")).toEntity
+      cl(Req(PUT, s"$iamBase/acls/", headersGroup, json)).mapResp { result =>
+        result.status shouldEqual StatusCodes.OK
+        result.entity.isKnownEmpty() shouldEqual true
+      }
+    }
+
+    "fail when wrong revision is provided" in {
+      eventually {
+        cl(Req(PUT, s"$adminBase/orgs/$id?rev=4", headersUser, create)).mapJson { (json, result) =>
+          result.status shouldEqual StatusCodes.Conflict
+          json shouldEqual jsonContentOf("/admin/errors/incorrect-revision.json", errorCtx)
+        }
+      }
+    }
+
+    "fail when project does not exist" in {
+      cl(Req(PUT, s"$adminBase/orgs/${genId()}?rev=1", headersUser, create)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.NotFound
+        json shouldEqual jsonContentOf("/admin/errors/not-exists.json", errorCtx)
+      }
+    }
+
+    "succeed and fetch revisions" in {
+
+      val updatedName = genString()
+      val update      = orgReqEntity(updatedName)
+
+      cl(Req(PUT, s"$adminBase/orgs/$id?rev=1", headersUser, update)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        json shouldEqual createRespJson(id, 2L, "orgs")
+      }
+
+      val updatedName2 = genString()
+      val update2      = orgReqEntity(updatedName2)
+
+      cl(Req(PUT, s"$adminBase/orgs/$id?rev=2", headersUser, update2)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        json shouldEqual createRespJson(id, 3L, "orgs")
+
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        validateAdminResource(json, "Organization", "orgs", id, updatedName2, 3L)
+
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id?rev=3", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        validateAdminResource(json, "Organization", "orgs", id, updatedName2, 3L)
+
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id?rev=2", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        validateAdminResource(json, "Organization", "orgs", id, updatedName, 2L)
+
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id?rev=1", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        validateAdminResource(json, "Organization", "orgs", id, name, 1L)
+      }
+    }
+
+  }
+
+  "deprecating an organization" should {
+    val id     = genId()
+    val name   = genString()
+    val create = orgReqEntity(name)
+
+    "fail when wrong revision is provided" in {
+      cl(Req(PUT, s"$adminBase/orgs/$id", headersUser, create)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.Created
+        json shouldEqual createRespJson(id, 1L, "orgs")
+      }
+
+      cl(Req(DELETE, s"$adminBase/orgs/$id?rev=4", headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.Conflict
+        json shouldEqual jsonContentOf("/admin/errors/incorrect-revision.json", errorCtx)
+      }
+    }
+
+    "fail when project does not exist" in {
+      cl(Req(DELETE, s"$adminBase/orgs/${genId()}?rev=1", headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.NotFound
+        json shouldEqual jsonContentOf("/admin/errors/not-exists.json", errorCtx)
+      }
+    }
+
+    "fail when revision is not provided" in {
+      cl(Req(DELETE, s"$adminBase/orgs/$id", headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.BadRequest
+        json shouldEqual jsonContentOf("/admin/errors/rev-not-provided.json", errorCtx)
+      }
+    }
+
+    "succeed if organization exists" in {
+      cl(Req(DELETE, s"$adminBase/orgs/$id?rev=1", headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        json shouldEqual createRespJson(id, 2L, "orgs")
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        validateAdminResource(json, "Organization", "orgs", id, name, 2L, true)
+
+      }
+
+      cl(Req(uri = s"$adminBase/orgs/$id?rev=1", headers = headersUser)).mapJson { (json, result) =>
+        result.status shouldEqual StatusCodes.OK
+        validateAdminResource(json, "Organization", "orgs", id, name, 1L)
+
+      }
+    }
+  }
+}
